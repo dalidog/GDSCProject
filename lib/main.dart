91e5ae7c2349c21a91c3e00517739981190eda3b
Dalia Sebat <daliasebat@ucsb.edu>
2023-02-02 23:43:24 -0800

    fixed flutter issue

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
