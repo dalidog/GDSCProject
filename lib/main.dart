17225270d00aa69bcd4ea46813f9eaa11a4b5ca1
Dalia Sebat <daliasebat@ucsb.edu>
2023-02-24 12:58:44 -0800

    now overlayed, need to fix positioning

--- lib/main.dart
+++ lib/main.dart
@@ -111,39 +111,28 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                constraints: BoxConstraints.expand(),
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("images/gdsc_background.jpg"),
-                      fit: BoxFit.cover),
-                ),
-              ),
+      body:
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          Stack(
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/gdsc_background.jpg"),
+                  fit: BoxFit.fitWidth),
             ),
-            Expanded(child: MyWidget()),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment(2, 3),
+            child: Container(child: MyWidget()),
+          ),
+          Container(
+            width: 80,
+            height: 80,
+            color: Colors.blue,
+          ),
+        ],
       ),
       //floatingActionButton: FloatingActionButton(
       //onPressed: _incrementCounter,
