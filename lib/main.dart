e7533b26b314bb7b634e419feb147a49571f5958
Dalia Sebat <daliasebat@ucsb.edu>
2023-03-09 02:02:40 -0800

    added all components, tbd: fix wall size, stop negative money, add water & timer & suggest text box

--- lib/main.dart
+++ lib/main.dart
@@ -76,12 +76,12 @@ class MyWidgetState extends State<MyWidget> {
 
     var houseImage2 = images['images/house2.png'];
     Sprite house2 = Sprite.fromImage(houseImage2!);
-    house2.position = const Offset(1000, 0);
+    house2.position = const Offset(500, -50);
     rootNode.addChild(house2);
 
     var houseImage3 = images['images/house3.png'];
     Sprite house3 = Sprite.fromImage(houseImage3!);
-    house3.position = const Offset(500, -50);
+    house3.position = const Offset(1000, 0);
     rootNode.addChild(house3);
 
     var houseImage4 = images['images/house4.png'];
@@ -93,6 +93,10 @@ class MyWidgetState extends State<MyWidget> {
 
 class MyHomePageState extends State<MyHomePage> {
   int counter = 1000; //the starting value
+  ImageMap images = ImageMap();
+  int treeOffset = -250;
+  int duneOffset = -400;
+  int wallOffset = -500;
 
   void incrementCounterBy(int val) {
     setState(() {
@@ -107,7 +111,52 @@ class MyHomePageState extends State<MyHomePage> {
   }
 
   void deleteHouse() {
-    rootNode.removeChild(rootNode.children[0]);
+    if (rootNode.children.isNotEmpty) {
+      for (final child in rootNode.children) {
+        if (child.position.dx % 500 == 0) {
+          rootNode.removeChild(child);
+          break;
+        }
+      }
+    }
+  }
+
+  void plantTree() async {
+    await images.load([
+      'images/tree.png',
+    ]);
+    // Access a loaded image from the ImageMap
+    var treeImage1 = images['images/tree.png'];
+    Sprite tree1 = Sprite.fromImage(treeImage1!);
+    tree1.position = Offset(treeOffset + 150, 0);
+    rootNode.addChild(tree1);
+    treeOffset += 150;
+  }
+
+  makeSand() async {
+    await images.load([
+      'images/dune.png',
+    ]);
+    // Access a loaded image from the ImageMap
+    var duneImage1 = images['images/dune.png'];
+    Sprite dune1 = Sprite.fromImage(duneImage1!);
+    dune1.position = Offset(duneOffset + 350, 550);
+    rootNode.addChild(dune1);
+    duneOffset += 350;
+  }
+
+  makeWall() async {
+    await images.load([
+      'images/wall.png',
+    ]);
+    // Access a loaded image from the ImageMap
+    for (int i = 0; i < 4; i++) {
+      var wallImage1 = images['images/wall.png'];
+      Sprite wall1 = Sprite.fromImage(wallImage1!);
+      wall1.position = Offset(wallOffset + 600, 950);
+      rootNode.addChild(wall1);
+      wallOffset += 600;
+    }
   }
 
   @override
@@ -123,13 +172,32 @@ class MyHomePageState extends State<MyHomePage> {
         color: Colors.white,
         child: Column(
           children: <Widget>[
-            Align(
-              alignment: Alignment.topLeft,
-              child: FloatingActionButton.extended(
-                onPressed: () =>
-                    {incrementCounterBy(-100), deleteHouse()},
-                tooltip: 'buy __',
-                label: const Text('relocate 1 house'),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: <Widget>[
+                  FloatingActionButton.extended(
+                    onPressed: () => {incrementCounterBy(-100), deleteHouse()},
+                    tooltip: '\$100',
+                    label: const Text('relocate 10 houses:'),
+                  ),
+                  FloatingActionButton.extended(
+                    onPressed: () => {incrementCounterBy(-50), plantTree()},
+                    tooltip: '\$50',
+                    label: const Text('plant 10 trees'),
+                  ),
+                  FloatingActionButton.extended(
+                    onPressed: () => {incrementCounterBy(-25), makeSand()},
+                    tooltip: '\$25',
+                    label: const Text('create sand dune'),
+                  ),
+                  FloatingActionButton.extended(
+                    onPressed: () => {incrementCounterBy(-500), makeWall()},
+                    tooltip: '\$500',
+                    label: const Text('build sea wall'),
+                  ),
+                ],
               ),
             ),
             Stack(
@@ -156,7 +224,7 @@ class MyHomePageState extends State<MyHomePage> {
               ],
             ),
             Text(
-              '\$$counter',
+              'Balance: \$$counter',
               //style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
